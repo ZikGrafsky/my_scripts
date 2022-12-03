4f6163f4c607a8f4836facea24e7642826aef393
ZikGrafsky <silentzzephyr@gmail.com>
2022-12-03 08:25:49 +0300

    Upd messages

--- add_gettempl_to_alias.sh
+++ add_gettempl_to_alias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp getTemplate.sh ~/.gettemplate
-echo "getTemplate copied to ~/"
+printf "└─ \033[32;1mgetTemplate copied to ~/\033[0m\n"
 chmod 777 ~/.gettemplate
 printf "alias mkt=\"sh ~/.gettemplate\"" >> ~/.bashrc
-echo "alias mkt has been added. Available options: mainc/maincc/make/script"
+printf "└─ \033[32;1malias mkt has been added. Available options: mainc/maincc/make/script.\033[0m"
